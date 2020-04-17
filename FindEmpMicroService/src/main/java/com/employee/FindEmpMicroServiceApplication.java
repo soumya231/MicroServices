f5012d6996e6b3c38239045222adef2bf1bb2893
soumya231 <soumyamalyala@gmail.com>
2020-04-17 17:21:55 +0530

    commit

---
+++ FindEmpMicroService/src/main/java/com/employee/FindEmpMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindEmpMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindEmpMicroServiceApplication.class, args);
+	}
+
+}
