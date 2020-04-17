7c8ae7e1e8bb98cb237e645fcef3283dca6fa719
soumya231 <soumyamalyala@gmail.com>
2020-04-17 17:23:15 +0530

    commit

---
+++ UpdateEmpMicroService/src/main/java/com/employee/UpdateEmpMicroServiceApplication.java
@@ -0,0 +1,23 @@
+package com.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@ComponentScan("com.employee")
+public class UpdateEmpMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdateEmpMicroServiceApplication.class, args);
+	}
+
+
+@Bean
+public RestTemplate getRestTemplate() {
+	return new RestTemplate();
+	
+}
+}
